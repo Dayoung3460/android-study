1db75cc55ed56cb0d29ecb60b084ad6fde554560
김다영 (클라라) <kimdayoung771@gmail.com>
2022-06-29 19:55:28 +0900

    spinner view

--- ViewStudy/app/src/main/java/wikibook/learnandriod/viewstudy/MainActivity.kt
+++ ViewStudy/app/src/main/java/wikibook/learnandriod/viewstudy/MainActivity.kt
@@ -1,12 +1,28 @@
 package wikibook.learnandriod.viewstudy
 
+import android.icu.number.Notation.simple
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.string_array, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val item = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity, "$position $item ${spinner.selectedItem.toString()}", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
