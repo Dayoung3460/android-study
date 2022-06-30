252112147b4f9bd787a20aa796cfbe9e1d2d49fe
김다영 (클라라) <kimdayoung771@gmail.com>
2022-06-30 17:08:13 +0900

    .

---
+++ TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteMainActivity.kt
@@ -0,0 +1,11 @@
+package wikibook.learnandriod.todayquote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuoteMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quote_activity_main)
+    }
+}
