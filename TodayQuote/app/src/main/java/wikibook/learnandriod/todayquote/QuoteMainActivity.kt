722a66783b0f8655f3f692588ea4a14fc629116f
김다영 (클라라) <kimdayoung771@gmail.com>
2022-06-30 17:22:16 +0900

    show random quote

--- TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteMainActivity.kt
+++ TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteMainActivity.kt
@@ -2,10 +2,29 @@ package wikibook.learnandriod.todayquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.util.*
+
 
 class QuoteMainActivity : AppCompatActivity() {
+    private lateinit var quotes: MutableList<Quote>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quote_activity_main)
+
+        val quoteText = findViewById<TextView>(R.id.quote_text)
+        val quoteFrom = findViewById<TextView>(R.id.quote_from)
+
+        quotes = mutableListOf()
+        quotes.add(Quote(1, "명언1", "출처1"))
+        quotes.add(Quote(2, "명언2", "출처2"))
+        quotes.add(Quote(3, "명언3", "출처3"))
+
+        val randomIndex = Random().nextInt(quotes.size)
+        val randomQuote = quotes[randomIndex]
+
+        quoteText.text = randomQuote.text
+        quoteFrom.text = randomQuote.from
     }
 }
