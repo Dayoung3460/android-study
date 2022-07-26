c702669b598359284c948a65db9577defbe857bf
Clara <kimdayoung771@gmail.com>
2022-07-26 20:51:39 +0900

    set layout manager

--- TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteListActivity.kt
+++ TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteListActivity.kt
@@ -1,8 +1,11 @@
 package wikibook.learnandriod.todayquote
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class QuoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,16 @@ class QuoteListActivity : AppCompatActivity() {
 
         val currentQuoteSize = intent.getIntExtra("quote_size", 0)
         Toast.makeText(this, "You have ${currentQuoteSize} quotes stored.", Toast.LENGTH_SHORT).show()
+
+        val pref = getSharedPreferences("quotes", Context.MODE_PRIVATE)
+
+        val quotes = Quote.getQuotesFromPreference(pref)
+        val layoutManager = LinearLayoutManager(this)
+        val adapter = QuoteAdapter(quotes)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.quote_list)
+        recyclerView.setHasFixedSize(false)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
     }
 }
