1c14d4cca510f748d87383976bcbcb4e7d87ac72
Clara <kimdayoung771@gmail.com>
2022-07-26 20:35:50 +0900

    get data from Intent

--- TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteListActivity.kt
+++ TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteListActivity.kt
@@ -2,10 +2,14 @@ package wikibook.learnandriod.todayquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class QuoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quote_list_activity)
+
+        val currentQuoteSize = intent.getIntExtra("quote_size", 0)
+        Toast.makeText(this, "You have ${currentQuoteSize} quotes stored.", Toast.LENGTH_SHORT).show()
     }
 }
