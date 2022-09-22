ee25e747f15af5bf5671b6ba86c28ee44cc9aec1
Clara <kimdayoung771@gmail.com>
2022-09-22 20:04:53 +0900

    fragment study init

--- TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteEditActivity.kt
+++ TodayQuote/app/src/main/java/wikibook/learnandriod/todayquote/QuoteEditActivity.kt
@@ -1,7 +1,6 @@
 package wikibook.learnandriod.todayquote
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
