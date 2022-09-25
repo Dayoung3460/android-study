15200d6b3cc4696208eef87d52f9e70ec9c8b40d
Clara <kimdayoung771@gmail.com>
2022-09-25 18:54:30 +0900

    modified: main activity

--- FragmentStudy/app/src/main/java/com/example/fragmentstudy/MainActivity.kt
+++ FragmentStudy/app/src/main/java/com/example/fragmentstudy/MainActivity.kt
@@ -8,7 +8,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container, CurrencyConverterFragment1())
+        transaction.add(R.id.fragment_container, CurrencyConverterFragment2.newInstance("KWR", "USD"))
+        transaction.add(R.id.fragment_container, CurrencyConverterFragment2.newInstance("JPY", "KRW"))
+        transaction.add(R.id.fragment_container, CurrencyConverterFragment2.newInstance("EUR", "JPY"))
         transaction.commit()
     }
 }
