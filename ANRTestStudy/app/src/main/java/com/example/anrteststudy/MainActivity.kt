04e132c348dd8d31b9c187513c35988903c04069
Clara <kimdayoung771@gmail.com>
2022-10-08 20:25:43 +0900

    added: show the progress

--- ANRTestStudy/app/src/main/java/com/example/anrteststudy/MainActivity.kt
+++ ANRTestStudy/app/src/main/java/com/example/anrteststudy/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.anrteststudy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 import java.lang.Math.sqrt
 import kotlin.random.Random
 
@@ -15,20 +18,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.anr_text_activity)
 
         var result = findViewById<TextView>(R.id.result)
+        val progressStatus = findViewById<TextView>(R.id.progress_status)
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        val progress = findViewById<TextView>(R.id.progress)
 
         findViewById<Button>(R.id.btn).setOnClickListener {
             Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
         }
 
         findViewById<Button>(R.id.anr).setOnClickListener {
+            result.text = ""
+            progressStatus.text = "Progressing"
+            progressBar.visibility = View.VISIBLE
+
             Thread(Runnable {
                 var sum = 0.0
-                for(i in 1 .. 60) {
+                var count = 0
+                for(i in 1 until 60) {
                     sum += sqrt(Random.nextDouble())
                     Thread.sleep(100)
+                    runOnUiThread {
+                        progress.text = "%.1f".format(((count + 1) / 60.toDouble()) * 100) + "% completed"
+                    }
+                    count++
                 }
                 runOnUiThread {
                     result.text = sum.toString()
+                    progressStatus.text = "Completed"
+                    progressBar.visibility = View.GONE
                 }
 
             }).start()
