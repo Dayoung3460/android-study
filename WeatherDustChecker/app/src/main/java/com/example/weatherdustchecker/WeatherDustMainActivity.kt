68d99032e596b71319ba2a3bdde7720028e323b6
Clara <kimdayoung771@gmail.com>
2022-10-25 20:35:10 +0900

    modified: apply view pager fragment to activity

---
+++ WeatherDustChecker/app/src/main/java/com/example/weatherdustchecker/WeatherDustMainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.weatherdustchecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+
+class WeatherDustMainActivity : AppCompatActivity() {
+    private lateinit var mPager: ViewPager
+    private var lat: Double = 37.579876
+    private var lon: Double = 126.976998
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.weather_dust_main_activity)
+        supportActionBar?.hide()
+
+        mPager = findViewById(R.id.pager)
+        val pagerAdapter = MyPagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+
+        mPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {}
+
+            override fun onPageSelected(position: Int) {
+                if(position === 0) {
+                    Toast.makeText(applicationContext, "Weather page", Toast.LENGTH_SHORT).show()
+                } else if(position === 1) {
+                    Toast.makeText(applicationContext, "Fine dust page", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+    private inner class MyPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int = 2
+        override fun getItem(position: Int): Fragment {
+            return when(position) {
+                0 -> WeatherPageFragment.newInstance(lat, lon)
+                1 -> DustPageFragment.newInstance(lat, lon)
+                else -> {
+                    throw Exception("Not found")
+                }
+            }
+        }
+    }
+}
