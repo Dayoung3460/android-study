98d97507ef24d28c9ca478b8644fc3e5cfa24d11
Clara <kimdayoung771@gmail.com>
2022-10-25 20:47:39 +0900

    modified: location permission request

--- WeatherDustChecker/app/src/main/java/com/example/weatherdustchecker/WeatherDustMainActivity.kt
+++ WeatherDustChecker/app/src/main/java/com/example/weatherdustchecker/WeatherDustMainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.weatherdustchecker
 
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,8 +15,13 @@ import androidx.viewpager.widget.ViewPager
 
 class WeatherDustMainActivity : AppCompatActivity() {
     private lateinit var mPager: ViewPager
-    private var lat: Double = 37.579876
-    private var lon: Double = 126.976998
+    private var lat: Double = 0.0
+    private var lon: Double = 0.0
+
+    private lateinit var locationManager: FragmentManager
+    private lateinit var locationListener: LocationListener
+
+    private val PERMISSION_REQUEST_CODE: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,36 @@ class WeatherDustMainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         mPager = findViewById(R.id.pager)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object: LocationListener {
+            override fun onLocationChanged(p0: Location) {
+                lat = location.latitude
+                lon = location.longitude
+                locationManager.removeUpdates(this)
+
+                val pagerAdapter = MyPagerAdapter(supportFragmentManager)
+                mPager.adapter = pagerAdapter
+                mPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+                    override fun onPageScrollStateChanged(state: Int) {}
+                    override fun onPageScrolled(
+                        position: Int,
+                        positionOffset: Float,
+                        positionOffsetPixels: Int
+                    ) {}
+
+                    override fun onPageSelected(position: Int) {
+                        if(position === 0) {
+                             Toast.makeText(applicationContext, "Weather page", Toast.LENGTH_SHORT).show()
+                        } else if(position === 1) {
+                            Toast.makeText(applicationContext, "Fine dust page", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+            }
+
+        }
+
         val pagerAdapter = MyPagerAdapter(supportFragmentManager)
         mPager.adapter = pagerAdapter
 
